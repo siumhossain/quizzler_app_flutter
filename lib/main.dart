9f9123621f762a2da284f80aefb4b217ce16b111
siumhossain <sium.hossain@yahoo.com>
2021-02-22 15:42:05 +0600

    update:display question

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  int questionNum = 0;
+  List<Icon> score= [];
+  List<String> question = [
+    'Do you love me?',
+    'Cse is a sucks subject',
+    'Do you love your life'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(question[questionNum],textAlign: TextAlign.center,style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.white,
+              ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child:
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: (){
+                setState(() {
+                  questionNum++;
+                  score.add(
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    )
+                  );
+                });
+                //function
+              },
+            ),
+        ),
+        ),
+        Expanded(
+          child:
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: (){
+                setState(() {
+                  questionNum++;
+                  score.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      )
+                  );
+                }); //function
+              },
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: score,
+          ),
+        )
+      ],
+    );
+
+  }
+}
+
